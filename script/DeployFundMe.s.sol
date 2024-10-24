7802cceb86cf424649e6c992f7111d6511efa18b
UstamY <haotian.y@yahooo.com>
2024-10-23 22:43:45 -0700

    my first commit

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
+
+contract DeployFundMe is Script {
+    function run() external returns (FundMe) {
+        //Before stratBroadcast -> not a "real" tx
+        HelperConfig helperconfig = new HelperConfig();
+        address ethUsdPriceFeed = helperconfig.activeNetworkConfig();
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
+        vm.stopBroadcast();
+        return fundMe;
+    }
+}
